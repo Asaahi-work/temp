56d800b7f02abf37db838b11e36b451381d1156a
Oleg Aleshin <Asaahi.work@gmail.com>
2024-11-21 13:06:15 +0300

    ds

--- app/page.tsx
+++ app/page.tsx
@@ -10,13 +10,13 @@ export default function Home() {
 
     <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsum rem, nostrum dolorem deleniti molestias tempore laboriosam totam. Debitis dignissimos enim, sequi voluptas nesciunt ullam mollitia ipsa. Voluptatibus beatae ullam explicabo.</p>
 
-    <img src="pic.png" alt="cv" height={500} width={450}/>
+    <Image src="/pic.png" alt="cv" height={500} width={450}/>
       <h3 className="ml-20">Способ связи:</h3>
 
           <Link href="tel:+79817625457">+79817625457</Link> <br />
           <Link href="/">ссылка на сайт</Link>
     <div className="flex ">
-      <Image src="title.jpg" alt="cv" height={500} width={450}/>
+      <Image src="/title.jpg" alt="cv" height={500} width={450}/>
       <h3 className="fonr-bold text-[30px] mt-10 ml-36">я тут учусь!!</h3>
     </div>
    
